c279ffb3d43da1481d4e6c4a495f4138433fe731
Trey Evans <lewis.r.evans@gmail.com>
2025-08-15 11:31:37 -0400

    Update styling.

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -23,7 +23,7 @@ struct AppContext {
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header-light.svg");
+// const HEADER_SVG: Asset = asset!("/assets/header-light.svg");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 #[derive(Deserialize, Serialize)]
@@ -60,16 +60,20 @@ fn App() -> Element {
 pub fn Hero() -> Element {
     rsx! {
         div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+            class: "relative isolate mt-8 max-w-3/4 place-content-center",
+                div {
+                    class: "text-center",
+                    div {
+                        h1 {
+                            "Welcome to Localstack Viewer!"
+                        }
+                        p {
+                            class: "text-lg pt-3",
+                            "This application provides the ability to browse look at localstack status and contents.\
+                            It covers cases not available in the default LocalStack UI - or that are normally only in the paid versions."
+                        }
+                    }
+                }
         }
     }
 }
